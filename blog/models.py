49225c0d4db4bf3b3007823acaf82fb328b15053
Yuliana <yuliana.vladimirovna.02@bk.ru>
2023-03-24 12:42:24 +0300

    Commit

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,24 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200) # заголовок
+    title = models.CharField(max_length=200)  # заголовок
     author = models.ForeignKey(  # автор (отношение многие-к-одному)
         "auth.User",
         on_delete=models.CASCADE,
     )
     body = models.TextField()
 
+    def __str__(self):
+        return self.title
 
-def __str__(self):
-    return self.title
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
 
 
-def get_absolute_url(self):
-    return reverse("post_detail", kwargs={"pk": self.pk})
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,37 @@
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView # new
 from django.views.generic.edit import CreateView, UpdateView, DeleteView  # new
 from django.urls import reverse_lazy # new
 
 from .models import Post
+from .forms import CommentForm
+
+
+def post_detail(request, post):
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,
+                  'post_detail.html',
+                 {'post': post,
+                  'comments': comments,
+                  'comment_form': comment_form})
 
 
 class BlogListView(ListView):
+    paginate_by = 3
     model = Post
     template_name = "home.html"
 
